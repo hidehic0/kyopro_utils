b1c74b167f694d080aa9ab2e7c6b4f0f93f9e970
hidehic0 <hidehico.ooba@gmail.com>
2024-10-01 11:39:45 +0900

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kyopro_utils",
+    version="1.0",
+    packages=find_packages(),
+    license="MIT",
+    author="hidehic0",
+    description="競プロで何かと使う関数を作ってみました !Atcoderなどでは使えません \n We have created useful functions that you might use in competition programming. !Cannot be used with codeforces, etc.",
+)
